7dc9947d838319c129f9ac0aca7a4378290724b0
thw <the.w.3107@gmail.com>
2024-12-20 17:02:35 -0300

    feat: implemented a chart to show knownledge

---
+++ src/components/Charts/Container.tsx
@@ -0,0 +1,24 @@
+import { Flex, Heading } from "@/once-ui/components";
+import React, { ReactElement } from "react";
+import { ResponsiveContainer } from "recharts";
+
+type Props = { title?: string; children: ReactElement };
+export default function ChartContainer(props: Props) {
+  return (
+    <Flex fillHeight fillWidth direction="column">
+      {props.title && (
+        <Heading wrap="balance" variant="display-strong-s">
+          {props.title}
+        </Heading>
+      )}
+      <ResponsiveContainer width="100%" height="100%">
+        {props.children}
+      </ResponsiveContainer>
+    </Flex>
+  );
+}
+
+export const colors = {
+  stroke: "#56ECAD",
+  fill: "#8884d8",
+};
